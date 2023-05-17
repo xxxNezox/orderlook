421139389ff01eabe42d96df8506e5c8dd101445
Timofey Natalin <121451303+xxxNezox@users.noreply.github.com>
2023-05-17 23:13:02 +0300

    Update RegistrationAPI.js

--- src/API/RegistrationAPI.js
+++ src/API/RegistrationAPI.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs')
+const path = require('path')
+
 app.use(express.json());
 
 app.post('/api/registration', (req, res) => {
-  const { name, email, password } = req.body;
-  console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+  const {restaurantName, adminPassword, employeePassword} = req.body;
+  console.log(`Restaurant: ${restaurantName}, Admin: ${adminPassword}, Employee: ${employeePassword}`);
   res.json({ message: 'Registration successful!' });
 });
 
